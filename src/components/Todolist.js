4a2d342e59b5757748b744efc356d964f29d97df
core.pager=cat <mejriimene501@gmail.com>
2021-09-20 00:59:13 +0300

    hello

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -2,18 +2,19 @@ import React ,{ useEffect } from 'react';
 import Todo from './todo';
 import { useSelector ,useDispatch} from 'react-redux';
 import { FilterHandler } from '../Redux/action';
-import { FILTER } from '../Redux/action-types';
+import filter from './Filter'
+
 
 export default function Todolist() {
    
     const dispatch=useDispatch()
     const todos =useSelector(state=>state.todos)
     const filteredTodos=useSelector(state=>state.filteredTodos) 
-    const filter=useSelector(state=>state.FILTER)
+    const FILTER=useSelector(state=>state.Filter)
 
     useEffect(()=>{
         dispatch(FilterHandler())
-  },[FILTER,todos])
+  },[filter,todos])
 
     return (
         <div>
